ef88551232ba29c5eeedb7d821445728b1d20b1c
= <=>
2020-07-03 17:53:36 +0100

    Added Cloudinary Upload

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # CloudinaryPhP
 This is to setup cloudinary file upload on php.
+
+
+# Default Upload
+Use \Cloudinary\Uploader::upload("file location"); to upload the picture to cloudinary.
+This uploads to the default cloudinary home folder and assigns a random name to it.
+
+# Upload with parameters
+You can customize the upload by adding parameters, such as file name, file folder and others
+
+# Getting response
+To get the uploaded file details, assign your upload to a variable, a json respnse is returned to it which you can get the file upload details from
+
+Use a var_dump($response) to see all the returned keys and their values.
--- upload.php
+++ upload.php
@@ -14,5 +14,23 @@ require 'cloudinary/src/Api.php';
 ));
 
 
+//Upload File without parameters
+$response_without_parameters = \Cloudinary\Uploader::upload("file location"); //pass in file location/url
+
+/*response contains the details of the uploaded file for instance 
+to get the link to the uploaded file and the file name do the following */
+
+$file_name = $response_without_parameters['public_id']; //can be saved in the database to reference later
+$file_link = $response_without_parameters['url']; //can be saved in the database to reference later
+
+
+
+//Upload File with parameters
+$response_with_parameters =  \Cloudinary\Uploader::upload("file location", //pass in file location/url
+    array(
+    "folder" => "folder to upload to/", //cloudinary folder to upload the file to
+    "public_id" => "file name" //name to save the file as on cloudinary
+));
+
 
 ?>
